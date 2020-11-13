a3187409d0ca543257d51d4f6a1162c6ae752eef
JefferLindao <jefferlindao@gmail.com>
2020-11-13 15:10:31 -0500

    MongoDB: almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,23 @@
-const list = []
+const db = require('mongoose')
+const Model = require('./model')
+
+db.Promise = global.Promise
+
+db.connect('mongodb://db_user_platzivideos:platzivideo123@cluster0-shard-00-00-vmb31.mongodb.net:27017,cluster0-shard-00-01-vmb31.mongodb.net:27017/db_telegram?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+console.log('[db] Conectada con exito')
 
 function addMessage(message) {
-  list.push(message)
+  const myMessage = new Model(message)
+  myMessage.save()
 }
 
-function getMessage() {
-  return list
+async function getMessage() {
+  const messages = await Model.find()
+  return messages
 }
 
 module.exports = {
